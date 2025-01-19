277ebad43ecf1001fd5244774edb5ab40bf8f929
Keith Butler <keithbutler2001@gmail.com>
2025-01-19 16:05:38 +0000

    comments

--- Objects/Scripts/level_up_controller.gd
+++ Objects/Scripts/level_up_controller.gd
@@ -8,19 +8,29 @@ func _ready():
 	option1.text = "Number of turrets + 1"
 	option2.text = "Turret damage + 50%"
 	option3.text = "MOVEMENT SPEED + 20%"
+	visible = false
+
+func get_upgrades():
+	#TODO: check in filesystem for possible upgrades
+	# randomily pick one
+	# return and spawn in scene at players feetb
+	pass
 
 func _on_option_1_pressed():
 	base_stat_increase()
+	#TODO: spawn upgrade item instead
 	PlayerStats.num_turrets_placeable += 1
 	visible = false
 
 func _on_option_2_pressed():
 	base_stat_increase()
+	#TODO: spawn upgrade item instead
 	PlayerStats.turret_damage *= 1.5
 	visible = false
 
 func _on_option_3_pressed():
 	base_stat_increase()
+	#TODO: spawn upgrade item instead
 	PlayerStats.movement_speed *= 1.20
 	visible = false
 
