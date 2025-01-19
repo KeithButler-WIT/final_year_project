c9a9d166d7a229c7a6ca0199ca3590e291970f13
Keith Butler <keithbutler2001@gmail.com>
2025-01-19 16:01:11 +0000

    add comments

--- Objects/Scripts/hubworld.gd
+++ Objects/Scripts/hubworld.gd
@@ -2,6 +2,6 @@ extends Node3D
 
 
 func _ready():
-	PlayerStats.load_game()
-	PlayerStats.reset()
+	PlayerStats.load_game() # TODO: Load on start not of return
+	PlayerStats.reset() # TODO: reset on return
 	Dialogic.start("hubworld")
