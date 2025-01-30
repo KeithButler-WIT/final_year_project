8f8f59907fe7863373d11fd4bedefebb667359a7
Keith Butler <keithbutler2001@gmail.com>
2025-01-30 15:39:32 +0000

    start of enemy array

--- Objects/Scripts/Spawner.gd
+++ Objects/Scripts/Spawner.gd
@@ -1,8 +1,8 @@
 extends Node3D
 
-@export var packed_enemy: PackedScene
+@export var packed_enemy: Array[PackedScene]
 @onready var timer = $Timer
-@onready var camera : Camera3D = get_node("/root/Level/Player/Camera")
+@onready var camera : Camera3D = get_node("../Player/CameraArm/Camera")
 
 var enemies_remaining_to_spawn
 var enemies_killed_this_wave = 0
@@ -15,7 +15,8 @@ var current_wave_number = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	ResourceLoader.load_threaded_request(packed_enemy.resource_path)
+	for enemy in packed_enemy:
+		ResourceLoader.load_threaded_request(enemy.resource_path)
 	waves = $"Waves".get_children()
 	start_next_wave()
 	_update_timer_wait()
@@ -46,14 +47,11 @@ func _on_enemy_health_died_signal():
 func _on_timer_timeout():
 	_update_timer_wait()
 	if enemies_remaining_to_spawn:
-		ResourceLoader.load_threaded_request(packed_enemy.resource_path)
-		var enemy_scene = ResourceLoader.load_threaded_get(packed_enemy.resource_path)
+		ResourceLoader.load_threaded_request(packed_enemy[0].resource_path)
+		var enemy_scene = ResourceLoader.load_threaded_get(packed_enemy[0].resource_path)
 		var enemy = enemy_scene.instantiate()
 		enemy.position = get_random_position_off_screen()
-		#print(enemy.position)
 		connect_to_enemy_signals(enemy)
-		#var scene_root = get_parent()
-		#scene_root.add_child(enemy)
 		get_tree().current_scene.add_child(enemy)
 		#enemies_remaining_to_spawn -= 1
 	else:
@@ -85,4 +83,4 @@ func get_random_position_off_screen() -> Vector3 :
 		randz = int(rng.randi_range(0, screensize.y))
 		randx = -distance_outside_screen if rng.randi() % 2 == 0 else screensize.x + distance_outside_screen
 
-	return Vector3(randx, 2, randz)
+	return Vector3(randx, 3, randz)
