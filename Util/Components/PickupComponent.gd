7434adccead04cd58ee798aba843753dbfd9f653
Keith Butler <keithbutler2001@gmail.com>
2025-01-30 15:38:40 +0000

    tween

--- Util/Components/PickupComponent.gd
+++ Util/Components/PickupComponent.gd
@@ -1,5 +1,11 @@
 extends Node3D
 
+func _process(delta: float) -> void:
+	#$"../pickup_area".scale.x = PlayerStats.pickup_radius
+	#$"../pickup_area".scale.z = PlayerStats.pickup_radius
+	$"../pickup_area/CollisionShape3D".scale.x = PlayerStats.pickup_radius
+	$"../pickup_area/CollisionShape3D".scale.z = PlayerStats.pickup_radius
+
 
 func _on_pickup_area_body_entered(body: Node3D):
 	var exp = body.get_parent_node_3d()
@@ -9,9 +15,10 @@ func _on_pickup_area_body_entered(body: Node3D):
 	#cleanup(exp)
 
 func animate_pickup(body:Node3D): # TODO: Fix tween animation
-	var tween = get_tree().create_tween()#.bind_node(body).set_trans(Tween.TRANS_ELASTIC)
+	body.get_node("RigidBody3D").get_node("CollisionShape3D").disabled = true
+	var tween = get_tree().create_tween().bind_node(self)
 	tween.set_parallel(true)
-	tween.tween_property(body, "position", global_position, 1)
+	tween.tween_property(body, "position", global_position, 0.1)
 	if (tween.finished):
 		tween.tween_callback(body.queue_free)
 
