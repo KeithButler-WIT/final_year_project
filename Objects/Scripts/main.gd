313ada63a785118c4b2e65daef2b50e0a29b7f53
Keith Butler <keithbutler2001@gmail.com>
2025-01-30 15:39:51 +0000

    clean up + game timer

--- Objects/Scripts/main.gd
+++ Objects/Scripts/main.gd
@@ -6,26 +6,51 @@ extends Node3D
 @export var missionEnd : PackedScene
 @export var exitLadder : PackedScene
 
+@onready var exp_bar = $Control/ExpUi/Bar
+@onready var exp_label = $Control/ExpUi/Label
+@onready var hp_bar = $Control/HpUi/Bar
+@onready var hp_label = $Control/HpUi/Label
+
+@onready var timer = $Timer
+var time_start = 0
 
 func _ready():
-	pass
+	time_start = Time.get_ticks_msec()
+
+
+var sec_to_min = func(sec):
+	return sec / 60
+
+var msec_to_min = func(msec):
+	return msec / 60  / 60 / 60
 
 
 func _process(_delta):
 	update_exp_bar()
 	update_hp_bar()
+	#print("Time Left: %d" % msec_to_min.call(Time.get_ticks_msec() - time_start))
+	#match msec_to_min.call(Time.get_ticks_msec() - time_start):
+		#0: 
+			#print("ZERO MINUTE")
+		#1:
+			#print("ONE MINUTE")
 
+	#print("Time Left: %d" % sec_to_min.call(timer.time_left))sw
+
+# ON TIMEOUT SPAWN BIG BOSS
+# VARIOUS TIMERS TO SPAWN MINI BOSSES
+# TIMER ON WAVES SPAWNING
 
 func update_exp_bar():
-	$Control/ExpUi/Bar.value = PlayerStats.experience
-	$Control/ExpUi/Bar.max_value = PlayerStats.exp_to_next_level
-	$Control/ExpUi/Label.text = "LEVEL " + str(PlayerStats.level)
+	exp_bar.value = PlayerStats.experience
+	exp_bar.max_value = PlayerStats.exp_to_next_level
+	exp_label.text = "LEVEL " + str(PlayerStats.level)
 
 
 func update_hp_bar():
-	$Control/HpUi/Bar.value = PlayerStats.current_health
-	$Control/HpUi/Bar.max_value = PlayerStats.max_health
-	$Control/HpUi/Label.text = str(PlayerStats.current_health) + "/" + str(PlayerStats.max_health)
+	hp_bar.value = PlayerStats.current_health
+	hp_bar.max_value = PlayerStats.max_health
+	hp_label.text = str(PlayerStats.current_health) + "/" + str(PlayerStats.max_health)
 
 
 func _on_objective_complete() -> void:
