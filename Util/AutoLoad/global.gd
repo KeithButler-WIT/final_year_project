618d66522dd3d38963e28e57dc7045db5cf3a495
Keith Butler <keithbutler2001@gmail.com>
2025-02-19 14:04:49 +0000

    goto_loadingscreen

--- Util/AutoLoad/global.gd
+++ Util/AutoLoad/global.gd
@@ -3,12 +3,23 @@ extends Node
 const LOADING_SCREEN_PATH : String = "res://Scenes/loading_screen.tscn"
 
 var current_scene = null
+var next_scene = null
 
 
 func _ready():
 	ResourceLoader.load_threaded_request(LOADING_SCREEN_PATH)
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() - 1)
+
+
+func goto_loading_screen(scene:String) -> void:
+	next_scene = scene
+	goto_scene(LOADING_SCREEN_PATH)
+
+
+#func goto_loading_screen(next_scene:PackedScene) -> void:
+	#next_scene = scene
+	#goto_scene(LOADING_SCREEN_PATH)
 
 
 func goto_scene(path):
@@ -30,14 +41,14 @@ func _deferred_goto_scene(path):
 	get_tree().current_scene.free()
 
 	# Load the new scene.
-	var progress = []
-	ResourceLoader.load_threaded_get_status(path, progress)
-	print(progress[0]*100)
-	if progress[0] == 1:
-		var s = ResourceLoader.load_threaded_get(path)
-		# Instance the new scene.
-		current_scene = s.instantiate()
-		# Add it to the active scene, as child of root.
-		get_tree().root.add_child(current_scene)
-		# Optionally, to make it compatible with the SceneTree.change_scene_to_file() API.
-		get_tree().current_scene = current_scene
+	#var progress = []
+	#ResourceLoader.load_threaded_get_status(path, progress)
+	#print(progress[0]*100)
+	#if progress[0] == 1:
+		#var s = ResourceLoader.load_threaded_get(path)
+	# Instance the new scene.
+	current_scene = load(path).instantiate()
+	# Add it to the active scene, as child of root.
+	get_tree().root.add_child(current_scene)
+	# Optionally, to make it compatible with the SceneTree.change_scene_to_file() API.
+	get_tree().current_scene = current_scene
