8ac1dd04d69729f56b0112b138f0699d125c28de
Keith Butler <keithbutler2001@gmail.com>
2025-02-24 09:43:12 +0000

    push builds to itch script

---
+++ push_builds.sh
@@ -0,0 +1,7 @@
+#!/bin/env sh
+
+# using [https://itchio.itch.io/butler]
+
+/home/keith/Downloads/butler/butler push builds/linux keith-butler/final-year-project:linux
+/home/keith/Downloads/butler/butler push builds/win keith-butler/final-year-project:win
+/home/keith/Downloads/butler/butler push builds/mac keith-butler/final-year-project:mac
